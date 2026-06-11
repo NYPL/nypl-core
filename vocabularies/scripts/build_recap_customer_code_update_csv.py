716e0bd7d97bed0d017ce6139c9ca7c9d4f21ee8
Paul Beaudoin <contact@paulbeaudoin.com>
2026-06-10 22:21:32 -0400

    Improved script, resynced customer codes

--- vocabularies/scripts/build_recap_customer_code_update_csv.py
+++ vocabularies/scripts/build_recap_customer_code_update_csv.py
@@ -1,12 +1,29 @@
+# Build a "update csv" from the ReCAP Customer Codes sheet
+# ( https://docs.google.com/spreadsheets/d/1ewSv5EilkS8LR1Fx51AG7iJwn5o-ZYeynJiCGf48XpU/edit?gid=0#gid=0 )
+#
+# Usage:
+#     - Download CSV export of ReCAP Customer Codes sheet
+#     - Run:
+#         cd vocabularies/scripts
+#         pip install -r requirements.txt
+#         python build_recap_customer_code_update_csv.py google-sheet-export.csv --outfile update.csv
+#     - Inspect update.csv (potentially manually adding missing labels and other values)
+#     - Use update-csv.py to patch the recapCustomerCodes.csv:
+#         python update-csv.py recapCustomerCodes update.csv
+#     - Proceed with serialization and committing changes for review.
+
 import csv
 import argparse
 
 
 class SheetRow:
+    """
+    Representing a single row exported from the ReCAP Customer Codes Google Sheet
+    """
     def __str__(self):
         return f'{self.id()} ({self.institution})' \
             + (f', EDD enabled' if self.edd else '') \
-            + (f' => {', '.join(self.deliverable_to)}' if len(self.deliverable_to) else '')
+            + (f', deliverable to {', '.join(self.deliverable_to)}' if len(self.deliverable_to) else '')
 
     def id(self):
         prefix = f'{self.depository}:' if self.depository != 'recap' else ''
@@ -17,7 +34,7 @@ class SheetRow:
         inst = SheetRow()
 
         inst.depository = depository
-        inst.code = row['CUSTOMER CODE']
+        inst.code = row['CUSTOMER CODE'].rstrip()
         inst.institution = row['OWNING INSTITUTION']
         inst.label = row['DESCRIPTION']
         if inst.institution == 'NEW YORK':
@@ -30,20 +47,30 @@ class SheetRow:
 
 
 def spreadsheet_rows(path):
+    """
+    Get all spreadsheet rows for CSV path
+    """
     depository = 'recap'
     with open(path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         rows = []
         for row in reader:
             code = row.get('CUSTOMER CODE', '')
+            # The HD customer codes are the ones below this message:
             if code.startswith('HD LAS values start here'):
                 depository = 'hd'
-            elif len(code) == 2:
+            elif len(code) in [2, 3]:
+                # For now, customer codes expected to be 2 or 3 characters
                 row['depository'] = depository
                 rows.append(SheetRow.from_row(row, depository))
 
     # We only care about entries belonging to partner institutions:
-    rows = [r for r in rows if len(r.institution) and not r.institution.startswith('ReCAP')]
+    rows = [
+        r for r in rows
+        if len(r.institution) \
+        and not r.institution.startswith('ReCAP') \
+        and not r.institution.startswith('HD')
+    ]
 
     return rows
 
@@ -62,11 +89,13 @@ def write_outfile(rows, outfile):
         for row in rows_to_write:
             writer.writerow(row)
 
+    print(f'\nWrote to {outfile}')
+
 
 def main(args):
     rows = spreadsheet_rows(args.csvfile)
 
-    print('Rows:')
+    print('Built rows:')
     for r in rows:
         print(r)
 
@@ -77,7 +106,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('csvfile')
     parser.add_argument('-o', '--outfile')
-    parser.add_argument('-v', '--verbose', action='store_true')
 
     args = parser.parse_args()
 
